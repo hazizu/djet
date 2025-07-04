9e6cea8c06496527e09367ea23db1ed5e08e33a4
hazizu <hazizu@192.168.1.14>
2025-07-04 12:23:33 +0000

    suppression de log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'djetFront';
+
+  constructor(
+    private http:HttpClient
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+    this.http.get('./../../assets/config/environment.json').subscribe((res:any)=>{
+      if(res && res.mode === 'dev'){
+         console.warn(`🚨 Console output is disabled on production!`);
+          console.log = function (): void { };
+          console.debug = function (): void { };
+          console.warn = function (): void { };
+          console.info = function (): void { };
+      }
+    })
+    
+  }
 }
